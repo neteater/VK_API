4039965e2f5bdc7d557a3583e0b2d97ca09dcee4
neteater <vladimirkevorkov2@yandex.ru>
2014-12-30 13:08:18 +0400

    fixes

--- vkapi_java/src/vk_api.java
+++ vkapi_java/src/vk_api.java
@@ -24,7 +24,7 @@ public class vk_api {
 
         public static String access_token="";
         public static int client_id=1;
-        public static int client_secret=1;
+        public static String client_secret="";
         private boolean debug=false;
         public int test_mode=0;
         public String getAccess_token() {
@@ -33,100 +33,6 @@ public class vk_api {
         protected void Debug() {
             this.debug=!this.debug;
         }
-
-
-        class users {
-            public String get(String u_ids,String fields,String name_case) throws IOException {
-                String q="users.get?user_id="+u_ids+"&fields="+fields+"&name_case="+name_case;
-                return exec_method(q, true);
-            }
-            public String get(String u_ids) throws IOException {
-                String q="users.get?user_id="+u_ids;
-                return exec_method(q, false);
-             }
-
-           /* public String search(String q,int sort,int offset,int count,String fields,int city,int country,String hometown,
-                                 int sex, int status, int age_from,int age_to,int b_day,int b_month,int b_year,int online,
-                                 int has_photo,String interests,int group_id) {
-                    String s_q;
-                            if (q!=null) s_q="q="+q; else s_q="";
-                    String srt="&sort";
-
-            }*/
-            public String getSubscriptions(String u_id) throws IOException {
-                String q="users.getSubscriptions?user_id="+u_id;
-                return exec_method(q, false);
-            }
-            public String getSubscriptions(String u_id,String fields) throws IOException {
-                String q="users.getSubscriptions?user_id="+u_id+"&fields="+fields;
-                return exec_method(q, false);
-            }
-            public String getSubscriptions(String u_id,int extended,int offset,int count,String fields) throws IOException {
-                String q="users.getSubscriptions?user_id="+u_id+"&extended="+extended+"&offset="+offset+"&count="+count+"&fields="+fields;
-                return exec_method(q,false);
-            }
-            public String getFollowers(String u_id) throws IOException {
-                String q="users.getFollowers?user_id="+u_id;
-                return exec_method(q, false);
-            }
-            public String getFollowers(String u_id,String fields,String name_case) throws IOException {
-                String q="users.getFollowers?user_id="+u_id+"&fields="+fields+"&name_case="+name_case;
-                return exec_method(q, false);
-            }
-            public String getFollowers(String u_id,int offset,int count,String fields,String name_case) throws IOException {
-                String q="users.getFollowers?user_id="+u_id+"&offset="+offset+"&count="+count+"&fields="+fields+"&name_case="+name_case;
-                return exec_method(q, false);
-            }
-            public String report(String u_id,String type,String comment) throws IOException {
-                String q="users.report?user_id="+u_id+"&type="+type+"&comment="+comment;
-                return exec_method(q, true);
-            }
-            public String report(String u_id,String type) throws IOException {
-                String q="users.report?user_id="+u_id+"&type="+type;
-                return exec_method(q, true);
-            }
-            public String getNearby(Double latitude, Double longitude,int accuracy,int timeout,int radius,String fields,String name_case) throws IOException {
-                String q="users.getNearby?latitude="+latitude+"&longitude="+longitude+"&accuracy="+accuracy+"&timeout="+timeout+"&radius="+radius+"&fields="+fields+"&name_case="+name_case;
-                return exec_method(q, true);
-            }
-            public String getNearby(Double latitude, Double longitude,String fields,String name_case) throws IOException {
-                String q="users.getNearby?latitude="+latitude+"&longitude="+longitude+"&fields="+fields+"&name_case="+name_case;
-                return exec_method(q, true);
-            }
-         }
-
-        class auth {
-            public String checkPhone(String ph_no) throws IOException {
-                String q="auth.checkPhone?phone="+ph_no+"&client_id="+client_id+"&client_secret="+client_secret;
-                return exec_method(q,false);
-            }
-            public String signup(String f_name,String l_name,String ph_no,String pwd,boolean test_mode,boolean voice,int sex) throws IOException {
-                String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&password="+pwd+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex;
-                return exec_method(q,false);
-            }
-            public String signup(String f_name,String l_name,String ph_no,boolean test_mode,boolean voice,int sex) throws IOException {
-                String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex;
-                return exec_method(q,false);
-            }
-            public String signup(String f_name,String l_name,String ph_no,String pwd,boolean test_mode,boolean voice,int sex,String sid) throws IOException {
-                String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&password="+pwd+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex+"&sid="+sid;
-                return exec_method(q,false);
-            }
-            public String signup(String f_name,String l_name,String ph_no,boolean test_mode,boolean voice,int sex,String sid) throws IOException {
-                String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex+"&sid="+sid;
-                return exec_method(q,false);
-            }
-            public String confirm(String ph_no,String code,String pwd,boolean test_mode,int intro) throws IOException {
-                String q="auth.conform?client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&code="+code+"&password="+pwd+"&test_mode="+test_mode+"intro="+intro;
-                return exec_method(q,false);
-            }
-            public String confirm(String ph_no,String code,boolean test_mode,int intro) throws IOException {
-                String q="auth.conform?client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&code="+code+"&test_mode="+test_mode+"intro="+intro;
-                return exec_method(q,false);
-            }
-            //restore will be here
-        }
-
 
     private String readStreamToString(InputStream in,String encoding) throws IOException {
         StringBuffer buffer=new StringBuffer();
@@ -139,3 +45,95 @@ public class vk_api {
     }
 
 }
+
+class users extends vk_api {
+         public String get(String u_ids,String fields,String name_case) throws IOException {
+             String q="users.get?user_id="+u_ids+"&fields="+fields+"&name_case="+name_case;
+             return exec_method(q, true);
+         }
+         public  String get(String u_ids) throws IOException {
+             String q="users.get?user_id="+u_ids;
+             return exec_method(q, false);
+          }
+
+        /* public String search(String q,int sort,int offset,int count,String fields,int city,int country,String hometown,
+                              int sex, int status, int age_from,int age_to,int b_day,int b_month,int b_year,int online,
+                              int has_photo,String interests,int group_id) {
+                 String s_q;
+                         if (q!=null) s_q="q="+q; else s_q="";
+                 String srt="&sort";
+
+         }*/
+         public String getSubscriptions(String u_id) throws IOException {
+             String q="users.getSubscriptions?user_id="+u_id;
+             return exec_method(q, false);
+         }
+         public String getSubscriptions(String u_id,String fields) throws IOException {
+             String q="users.getSubscriptions?user_id="+u_id+"&fields="+fields;
+             return exec_method(q, false);
+         }
+         public String getSubscriptions(String u_id,int extended,int offset,int count,String fields) throws IOException {
+             String q="users.getSubscriptions?user_id="+u_id+"&extended="+extended+"&offset="+offset+"&count="+count+"&fields="+fields;
+             return exec_method(q,false);
+         }
+         public String getFollowers(String u_id) throws IOException {
+             String q="users.getFollowers?user_id="+u_id;
+             return exec_method(q, false);
+         }
+         public String getFollowers(String u_id,String fields,String name_case) throws IOException {
+             String q="users.getFollowers?user_id="+u_id+"&fields="+fields+"&name_case="+name_case;
+             return exec_method(q, false);
+         }
+         public String getFollowers(String u_id,int offset,int count,String fields,String name_case) throws IOException {
+             String q="users.getFollowers?user_id="+u_id+"&offset="+offset+"&count="+count+"&fields="+fields+"&name_case="+name_case;
+             return exec_method(q, false);
+         }
+         public String report(String u_id,String type,String comment) throws IOException {
+             String q="users.report?user_id="+u_id+"&type="+type+"&comment="+comment;
+             return exec_method(q, true);
+         }
+         public String report(String u_id,String type) throws IOException {
+             String q="users.report?user_id="+u_id+"&type="+type;
+             return exec_method(q, true);
+         }
+         public String getNearby(Double latitude, Double longitude,int accuracy,int timeout,int radius,String fields,String name_case) throws IOException {
+             String q="users.getNearby?latitude="+latitude+"&longitude="+longitude+"&accuracy="+accuracy+"&timeout="+timeout+"&radius="+radius+"&fields="+fields+"&name_case="+name_case;
+             return exec_method(q, true);
+         }
+         public String getNearby(Double latitude, Double longitude,String fields,String name_case) throws IOException {
+             String q="users.getNearby?latitude="+latitude+"&longitude="+longitude+"&fields="+fields+"&name_case="+name_case;
+             return exec_method(q, true);
+         }
+      }
+
+class auth extends vk_api {
+      public String checkPhone(String ph_no) throws IOException {
+          String q="auth.checkPhone?phone="+ph_no+"&client_id="+client_id+"&client_secret="+client_secret;
+          return exec_method(q,false);
+      }
+      public String signup(String f_name,String l_name,String ph_no,String pwd,boolean test_mode,boolean voice,int sex) throws IOException {
+          String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&password="+pwd+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex;
+          return exec_method(q,false);
+      }
+      public String signup(String f_name,String l_name,String ph_no,boolean test_mode,boolean voice,int sex) throws IOException {
+          String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex;
+          return exec_method(q,false);
+      }
+      public String signup(String f_name,String l_name,String ph_no,String pwd,boolean test_mode,boolean voice,int sex,String sid) throws IOException {
+          String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&password="+pwd+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex+"&sid="+sid;
+          return exec_method(q,false);
+      }
+      public String signup(String f_name,String l_name,String ph_no,boolean test_mode,boolean voice,int sex,String sid) throws IOException {
+          String q="auth.signup?first_name="+f_name+"&last_name="+l_name+"&client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&test_mode="+test_mode+"&voice="+voice+"&sex="+sex+"&sid="+sid;
+          return exec_method(q,false);
+      }
+      public String confirm(String ph_no,String code,String pwd,boolean test_mode,int intro) throws IOException {
+          String q="auth.conform?client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&code="+code+"&password="+pwd+"&test_mode="+test_mode+"intro="+intro;
+          return exec_method(q,false);
+      }
+      public String confirm(String ph_no,String code,boolean test_mode,int intro) throws IOException {
+          String q="auth.conform?client_id="+client_id+"&client_secret="+client_secret+"&phone="+ph_no+"&code="+code+"&test_mode="+test_mode+"intro="+intro;
+          return exec_method(q,false);
+      }
+      //restore will be here
+  }
